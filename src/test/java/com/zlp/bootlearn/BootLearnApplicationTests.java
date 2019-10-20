6838397b94d34fbe95f37af8b6200ecb8daaa311
tianyu1 <18222170921@163.com>
2019-10-20 18:29:27 +0800

    springboot 入门

---
+++ src/test/java/com/zlp/bootlearn/BootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlp.bootlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
